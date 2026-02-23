d868fda299c33dba134c99b3fc50105e1d01c0f6
Shaun McMors <s.mcmorn@btinternet.com>
2026-02-23 20:28:34 +0000

    Remove DVLA re-exports from calculateSettlement wrapper

--- app/lib/calculateSettlement.ts
+++ app/lib/calculateSettlement.ts
@@ -1,1 +1,5 @@
-export * from "../../lib/calculateSettlement";
+﻿// app/lib/calculateSettlement.ts
+// Backwards-compatible wrapper: re-export from /lib (AuctionMyCamera settlement logic)
+
+export { calculateSettlement } from "../../lib/calculateSettlement";
+export type { Settlement } from "../../lib/calculateSettlement";
